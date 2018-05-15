526bfa0248fe4cc027a633b1ef5bedaa467f4089
ness <ness55779@gmail.com>
2018-05-15 19:10:47 +0300

    Added logging to service layer

--- src/main/java/service/StudentServiceImpl.java
+++ src/main/java/service/StudentServiceImpl.java
@@ -1,6 +1,8 @@
 package service;
 
 import model.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import repository.StudentRepository;
 import repository.StudentSpecification;
 import repository.StudentSpecificationAll;
@@ -8,48 +10,103 @@ import repository.StudentSpecificationById;
 
 import java.util.List;
 
-public class StudentServiceImpl implements StudentService{
+public class StudentServiceImpl implements StudentService {
 
     private StudentRepository repository;
+    private Logger logger = LoggerFactory.getLogger(StudentServiceImpl.class);
 
-    public StudentServiceImpl(StudentRepository repository){
+    public StudentServiceImpl(StudentRepository repository) {
 
         this.repository = repository;
     }
 
     @Override
     public int addStudent(Student student) {
-        return repository.addStudent(student);
+        logger.info("Trying to add student " + student);
+
+        int result = repository.addStudent(student);
+
+        if (result == 1) {
+            logger.info("Student added");
+        } else {
+            logger.info("Student NOT added");
+        }
+        return result;
     }
 
     @Override
     public int updateStudent(Student student) {
-        return repository.updateStudent(student);
+        logger.info("Trying to update student " + student);
+
+        int result = repository.updateStudent(student);
+
+        if (result == 1) {
+            logger.info("Student updated");
+        } else {
+            logger.info("Student NOT updated");
+        }
+        return result;
     }
 
     @Override
     public int removeStudent(Student student) {
-        return repository.removeStudent(student);
+        logger.info("Trying to delete student " + student);
+
+        int result = repository.removeStudent(student);
+
+        if (result == 1) {
+            logger.info("Student deleted");
+        } else {
+            logger.info("Student NOT deleted");
+        }
+        return result;
     }
 
     @Override
     public int removeStudent(int id) {
-        return repository.removeStudent(id);
+        logger.info("Trying to delete student by id = " + id);
+
+        int result = repository.removeStudent(id);
+
+        if (result == 1) {
+            logger.info("Student deleted");
+        } else {
+            logger.info("Student NOT deleted");
+        }
+        return result;
     }
 
     @Override
     public Student getStudentById(int id) {
+
+        logger.info("Trying to find student by id = " + id);
+
         StudentSpecification specification = new StudentSpecificationById(id);
         List<Student> returnedList = repository.query(specification);
-        if(!returnedList.isEmpty()){
 
-            return returnedList.get(0);
+        if (!returnedList.isEmpty()) {
+
+            Student student = returnedList.get(0);
+            logger.info("Student found: " + student);
+            return student;
         }
+        logger.info("Student NOT found");
         return null;
     }
 
     @Override
     public List<Student> getStudents() {
-        return repository.query(new StudentSpecificationAll());
+
+        logger.info("Trying to get student list");
+
+        List<Student> list = repository.query(new StudentSpecificationAll());
+
+        if (list.isEmpty())
+            logger.info("List is empty");
+        else
+            logger.info("Got list");
+
+
+        return list;
     }
 }
